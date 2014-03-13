165c80402893b9adbd5f41d1c15696f31589642c
leops <leopaul3@gmail.com>
2014-03-13 21:34:30 +0100

    Update coffeelint.js
    Adds a JShint-like reporter option to choose how the results are presented.

--- tasks/coffeelint.js
+++ tasks/coffeelint.js
@@ -1,52 +1,57 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   var coffeelint = require('coffeelint');
 
-  grunt.registerMultiTask('coffeelint', 'Validate files with CoffeeLint', function() {
+  grunt.registerMultiTask('coffeelint', 'Validate files with CoffeeLint', function () {
 
     var errorCount = 0;
     var warnCount = 0;
     var files = this.filesSrc;
     var options = this.options();
+    var reporter = this.data.reporter;
 
     if (options.configFile != undefined) {
       var config = grunt.file.readJSON(options.configFile);
       options.configFile = undefined;
       for (var key in options) {
-          config[key] = options[key];
+        config[key] = options[key];
       }
       options = config;
     }
 
-    files.forEach(function(file) {
-      grunt.verbose.writeln('Linting ' + file + '...');
-
-      var literate = !!file.match(/\.(litcoffee|coffee\.md)$/i);
+    files.forEach(function (file) {
+      var literate = !! file.match(/\.(litcoffee|coffee\.md)$/i);
       var errors = coffeelint.lint(grunt.file.read(file), options, literate);
 
-      if (!errors.length) {
-        return grunt.verbose.ok();
-      }
+      if (reporter)
+        reporter(file, errors);
+      else {
+        grunt.verbose.writeln('Linting ' + file + '...');
 
-      errors.forEach(function(error) {
-        var status, message;
-
-        if (error.level === 'error') {
-          errorCount += 1;
-          status = "[error]".red;
-        } else if (error.level === 'warn') {
-          warnCount += 1;
-          status = "[warn]".yellow;
-        } else {
-          return;
+        if (!errors.length) {
+          return grunt.verbose.ok();
         }
 
-        message = file + ':' + error.lineNumber + ' ' + error.message +
+        errors.forEach(function (error) {
+          var status, message;
+
+          if (error.level === 'error') {
+            errorCount += 1;
+            status = "[error]".red;
+          } else if (error.level === 'warn') {
+            warnCount += 1;
+            status = "[warn]".yellow;
+          } else {
+            return;
+          }
+
+          message = file + ':' + error.lineNumber + ' ' + error.message +
             ' (' + error.rule + ')';
 
-        grunt.log.writeln(status + ' ' + message);
-        grunt.event.emit('coffeelint:' + error.level, error.level, message);
-        grunt.event.emit('coffeelint:any', error.level, message);
-      });
+          grunt.log.writeln(status + ' ' + message);
+          grunt.event.emit('coffeelint:' + error.level, error.level, message);
+          grunt.event.emit('coffeelint:any', error.level, message);
+        });
+      }
     });
 
     if (errorCount && !options.force) {
